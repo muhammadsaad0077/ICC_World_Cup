db28b579953322754844b9705a9e71e599b11867
muhammadsaad0077 <saaditanoli12@gmail.com>
2024-04-10 00:48:35 +0500

    Adding data and Slider

--- src/Components/Body.js
+++ src/Components/Body.js
@@ -1,12 +1,13 @@
 import React, { useEffect, useState } from 'react';
 import img from "../Images/icc world cup.png";
 import './styles.css'; // Import CSS file
-import img1 from '../Images/2009 pakistan.jpeg'
-import img2 from '../Images/2010 england.jpg'
+import { img10, img11, img12, img13, img14, img15, img16, img17, img18, img19, img20, img21, img22, img23, img24, img25, img26, img27, img28, img29, img30, img31, img32, img33 } from './const';
 
 const Body = () => {
   const years = [2022, 2021, 2016, 2014, 2012, 2010, 2009, 2007];
   const [activeYear, setActiveYear] = useState(years[0]);
+  const [activeImageIndex, setActiveImageIndex] = useState(0);
+
 
   useEffect(() => {
     const handleScroll = () => {
@@ -29,21 +30,29 @@ const Body = () => {
   }, [years]);
 
   const yearBackgrounds = {
-    2007: 'url("https://www.telegraph.co.uk/content/dam/cricket/2022/11/13/TELEMMGLPICT000316088724_1_trans_NvBQzQNjv4BqW5LAbqXSQw_G1yEaLnMlB15NUcTKNHKqy85KLqtvPLE.jpeg?imwidth=680")',
-    2009:  'url("https://img1.hscicdn.com/image/upload/f_auto/lsci/db/PICTURES/CMS/348500/348536.jpg")',
-    2010:  'url("https://e0.365dm.com/20/03/2048x1152/skysports-england-t20-world-cup_4949485.jpg?20200317150942")',
-    2012: 'url("https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEjP-st-ej8ypYLvqlAUN0cbypK-RIOwskan3p9QHKU7YVdB1V7Bo1OVUxt4xqzEcwraXy4lH_UHI4KXA3Q_pGtnspQ1230ooiV_VMxOlV8FwXYFh7jD3C9989OitTBFeBHzA5oI1U7miXs/s1600/sri-lanka-vs-west-indies-icc-world-t20-2012-final.jpg")',
-    2014: 'url("https://staticg.sportskeeda.com/editor/2022/10/44f29-16654886538765-1920.jpg")',
-    2016: 'url("https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEig32KCmx2T-DB6AWLb8mPxcpJ4_VNjPjH9s04vvqczD_2WYpKCv4jGYbBqtQLwIFXm_n5_gK3ymU_-KYBLp7-a_NjMKL2SGt5N8-_NfIGx2BeEl_UBwZ25psaj3wvYPTUzvISxg8sTY4M/s1600/3910869736001_4829498645001_4829398053001-vs.jpg")',
-    2021: 'url("https://sc0.blr1.cdn.digitaloceanspaces.com/article/180938-wylkwwjewk-1665136728.jpg")',
-    2022: 'url("https://www.boomlive.in/h-upload/2022/11/13/990678-england-icc-t20-world-cup-2022-1.webp")',
-    
+  2007: [ img32, img31, img33],    
+  2009: [ img30, img29, img28],    
+  2010: [ img26, img27, img25],    
+  2012: [ img22, img23, img24],    
+  2014: [ img19, img20, img21],    
+  2016: [ img16, img17, img18],    
+  2021: [ img13, img14, img15],    
+  2022: [ img10, img11, img12],    
   };
   
 
   const handleClick = (year)=>{
     setActiveYear(year)
+    setActiveImageIndex(0)
   }
+
+  const handleNextImage = () => {
+    setActiveImageIndex((prevIndex) => (prevIndex === 2 ? 0 : prevIndex + 1));
+  };
+
+  const handlePreviousImage = () => {
+    setActiveImageIndex((prevIndex) => (prevIndex === 0 ? 2 : prevIndex - 1));
+  };
 
   const renderData = () => {
     // Add logic here to return different components or data based on activeYear
@@ -82,31 +91,35 @@ const Body = () => {
     <div className="grid grid-cols-12" >
       <div className='bg-red-500 col-span-5 fixed h-[100%] w-[25%]'>
         <div className='flex'>
-          <img alt='img' src={img} className='m-4 ml-16 w-[60px] h-[60px] rounded-[60%]' />
+          <img alt='img' src={img} className='m-4 ml-4 w-[60px] h-[60px] rounded-[60%]' />
           <h1 className='text-[30px] mt-4 rounded-xl font-sans stroke-red-600 text-white font-bold whitespace-nowrap'>ICC WC WINNERS</h1>
         </div>
       </div>
       <div
-      className='col-span-7 bg-yellow-400 ml-[2.5px] relative left-[40%] h-screen w-[130%] transition-all transition-1000 ease-in-out'
+      className='col-span-7 bg-yellow-400 ml-[2.5px] relative left-[40%] h-screen w-[130%] transition-all transition-1000 ease-out'
       style={{
-        backgroundImage: yearBackgrounds[activeYear],
+        backgroundImage: `url(${yearBackgrounds[activeYear][activeImageIndex]})`,
         backgroundPosition: '',
         backgroundRepeat: '',
         backgroundSize: '',
-        opacity: '1.5'
+        opacity: '1.8'
       }}
     >
+      <div className='flex text-xl'>
+        <button className='text-white ml-2 cursor-default'>Next</button>
+        <img onClick={handleNextImage} alt='img' className='w-4 cursor-pointer h-4 relative top-2 ml-3' src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTyRLPlRYbFHmbXzjnP6pe2DeSaRX7wdQWe91z2tNmp-A&s'></img>
+      </div>
         {renderData()}
         {
           years.map((year, index) => (
             <div
               key={year}
               id={`year-${year}`}
-              className={`pointer mt-12 relative top-14 text-white right-20 ${year === activeYear ? 'text-7xl font-bold text-white drop-shadow-2xl right-[25%]' : 'text-[15px]'}`}
+              className={`pointer transition-all transition-1000 ease-in mt-12 relative top-14 text-white right-20 ${year === activeYear ? 'text-7xl font-bold text-white drop-shadow-2xl right-[25%]' : 'text-[15px]'}`}
               style={{ zIndex: years.length - index }}
               onClick={()=> handleClick(year)}
             >
-              <span className='year-element text-center p-2 relative top-2'>{year}</span>
+              <span className='year-element text-center p-2 relative top-2 cursor-pointer'>{year}</span>
             </div>
           ))
         }
---
+++ src/Components/const.js
@@ -0,0 +1,40 @@
+export const img10 = "https://www.telegraph.co.uk/content/dam/cricket/2022/11/13/TELEMMGLPICT000316088724_1_trans_NvBQzQNjv4BqW5LAbqXSQw_G1yEaLnMlB15NUcTKNHKqy85KLqtvPLE.jpeg?imwidth=680"
+export const img11 = "https://www.aljazeera.com/wp-content/uploads/2022/11/000_32NF3MT.jpg?resize=770%2C513&quality=80"
+export const img12 = "https://www.sportsmania.asia/wp-content/uploads/2022/12/sam-curran-afp-1161982-1668346847.jpeg"
+
+
+export const img13 = "https://sc0.blr1.cdn.digitaloceanspaces.com/article/180938-wylkwwjewk-1665136728.jpg"
+export const img14 = "https://images.news18.com/ibnlive/uploads/2021/11/australia-cricketers-1600-ap-new.jpg"
+export const img15 = "https://resources.cricket-australia.pulselive.com/photo-resources/2023/05/05/3c7c99c1-4900-4ad8-821a-d1ae9cbbe9af/7RQ9oVxh.ashx?width=950&height=535"
+
+
+export const img16 = "https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEig32KCmx2T-DB6AWLb8mPxcpJ4_VNjPjH9s04vvqczD_2WYpKCv4jGYbBqtQLwIFXm_n5_gK3ymU_-KYBLp7-a_NjMKL2SGt5N8-_NfIGx2BeEl_UBwZ25psaj3wvYPTUzvISxg8sTY4M/s1600/3910869736001_4829498645001_4829398053001-vs.jpg"
+export const img17 = "https://ichef.bbci.co.uk/onesport/cps/624/cpsprodpb/137DC/production/_89063897_trophy_reuters.jpg"
+export const img18 = "https://www.sportsadda.com/static-assets/waf-images/aa/21/a0/16-9/et0kFG7JQr.jpg?v=1.6&w=420%20420w"
+
+
+export const img19 = "https://3.bp.blogspot.com/-NfqLAH3_HKg/U0KjjaoAwRI/AAAAAAAABLQ/Dl-qiK3RWaQ/s1600/183511.jpg"
+export const img20 = "https://staticg.sportskeeda.com/editor/2022/10/44f29-16654886538765-1920.jpg"
+export const img21 = "https://images.news18.com/ibnlive/uploads/2022/04/on-this-day-cricket-srilanka-t20-world-cup.jpg"
+
+
+export const img22 = "https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEjP-st-ej8ypYLvqlAUN0cbypK-RIOwskan3p9QHKU7YVdB1V7Bo1OVUxt4xqzEcwraXy4lH_UHI4KXA3Q_pGtnspQ1230ooiV_VMxOlV8FwXYFh7jD3C9989OitTBFeBHzA5oI1U7miXs/s1600/sri-lanka-vs-west-indies-icc-world-t20-2012-final.jpg"
+export const img23 = "https://timesofindia.indiatimes.com/photo/16715195.cms"
+export const img24 = "https://ss-i.thgim.com/public/magazine/article37068151.ece/alternates/FREE_1200/ss-t20wc-2012"
+
+
+export const img25 = "https://e0.365dm.com/20/03/2048x1152/skysports-england-t20-world-cup_4949485.jpg?20200317150942"
+export const img26 = "https://www.sportsmirchi.com/wp-content/uploads/2015/09/England-Cricket-Team-at-ICC-World-Twenty20.jpg"
+export const img27 = "https://img1.hscicdn.com/image/upload/f_auto,t_ds_w_960,q_50/lsci/db/PICTURES/CMS/182400/182421.jpg"
+
+
+export const img28 = "https://akm-img-a-in.tosshub.com/indiatoday/images/story/202110/Pakistan_2009_1200x768.jpeg?size=1200:675"
+export const img29 = "https://i.dawn.com/large/2015/06/5586b08e7219d.jpg"
+export const img30 = "https://st1.latestly.com/wp-content/uploads/2020/06/GettyImages-88608148-1.jpg"
+
+
+export const img31 = "https://static.toiimg.com/thumb/resizemode-4,width-1280,height-720,msid-94416060/94416060.jpg"
+export const img32 = "https://cdn.dnaindia.com/sites/default/files/styles/half/public/2021/10/19/1001426-world-t20-2007.jpg"
+export const img33 = "https://queenmaryscollege.edu.in/blog/wp-content/uploads/2023/08/cricket-world-cup-2007_e5bd70085.jpg"
+
+
